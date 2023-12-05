26397559acbd72c0e8b32542aec58575a6b038e1
briannghiem <brian.nghiem@mail.utoronto.ca>
2023-12-05 15:59:14 -0500

    many changes; main thing is that grad was modified to evaluate relevant TR

--- BFGS.py
+++ BFGS.py
@@ -1,25 +1,9 @@
 import jax
 import jax.numpy as xp
 import matplotlib.pyplot as plt 
+from time import time
 
-def grad(f,x, h=1e-3): 
-    '''
-    CENTRAL FINITE DIFFERENCE CALCULATION
-    '''
-    # h = xp.cbrt(xp.finfo(float).eps)
-    d = len(x)
-    nabla = xp.zeros(d)
-    for i in range(d): 
-        x_for = x.at[i].set(x[i]+h) 
-        x_back = x.at[i].set(x[i]-h) 
-        f_for = f(x_for)
-        f_back = f(x_back)
-        f_dif = (f_for- f_back)/(2*h)
-        nabla = nabla.at[i].set(f_dif)
-        print("Dimension {} -- Finite dif: {}".format(i+1, f_dif))
-    return nabla 
-
-def line_search(f,x,p,nabla):
+def line_search(f,x,p,g):
     '''
     BACKTRACK LINE SEARCH WITH WOLFE CONDITIONS
     '''
@@ -28,15 +12,14 @@ def line_search(f,x,p,nabla):
     c2 = 0.9 
     fx = f(x)
     x_new = x + a * p 
-    nabla_new = grad(f,x_new)
-    while f(x_new) >= fx + (c1*a*nabla.T@p) or nabla_new.T@p <= c2*nabla.T@p : 
+    g_new = grad(f,x_new,args)
+    while f(x_new) >= fx + (c1*a*g.T@p) or g_new.T@p <= c2*g.T@p : 
         a *= 0.5
         x_new = x + a * p 
-        nabla_new = grad(f,x_new)
+        g_new = grad(f,x_new,args)
     return a
 
-
-def BFGS(f,x0,max_it):
+def BFGS(f,x0,args,max_it,spath):
     '''
     DESCRIPTION
     BFGS Quasi-Newton Method, implemented as described in Nocedal:
@@ -54,23 +37,30 @@ def BFGS(f,x0,max_it):
     x:      the optimal solution of the function f 
     #
     '''
+    #Initial values
     d = len(x0) # dimension of problem 
-    nabla = grad(f,x0) # initial gradient 
+    g = grad(f,x0,args) # initial gradient 
     H = xp.eye(d) # initial hessian
     x = x0[:]
-    it = 2 
-    while xp.linalg.norm(nabla) > 1e-5: # while gradient is positive
-        print("BFGS Iteration {}".format(it))
+    #Set up stores
+    x_store = []
+    f_store = []
+    g_store = []
+    #Run BFGS algorithm
+    it = 1 
+    while xp.linalg.norm(g) > 1e-5: # while gradient is positive
+        print("BFGS Iteration {}".format(it), end = '\n')
         if it > max_it: 
             print('Maximum iterations reached!')
             break
-        it += 1
-        p = -H@nabla # search direction (Newton Method)
-        a = line_search(f,x,p,nabla) # line search 
+        t1 = time()
+        p = -H@g # search direction (Newton Method)
+        # a = line_search(f,x,p,g) # line search 
+        a = 1e-4 #fixing step size
         s = a * p 
-        x_new = x + a * p 
-        nabla_new = grad(f,x_new)
-        y = nabla_new - nabla 
+        x_new = x + s
+        g_new = grad(f,x_new,args)
+        y = g_new - g 
         y = xp.array([y])
         s = xp.array([s])
         y = xp.reshape(y,(d,1))
@@ -80,13 +70,34 @@ def BFGS(f,x0,max_it):
         ri = (xp.eye(d)-(r*((y@(s.T)))))
         hess_inter = li@H@ri
         H = hess_inter + (r*((s@(s.T)))) # BFGS Update
-        nabla = nabla_new[:] 
+        g = g_new[:] 
         x = x_new[:]
-    return x
+        it += 1
+        #Save to store
+        x_store.append(x)
+        f_store.append(f(x))
+        g_store.append(g)
+        xp.save(spath + r'/opt_out.npy', [x_store, f_store, g_store])
+        t2 = time()
+        print("Elapsed time for BFGS Iter {}: {} sec".format(it, t2 - t1))
+    return x_store, f_store, g_store
 
 
 '''
+
 from functools import partial
+
+def _f_intra(Mtraj_est_n, m_est=None, C=None, res=None, U_shot=None, R_pad=None, s_corrupted=None):
+    #Data consistency for a given shot
+    n_shots = len(U_shot)
+    s_n = eop.Encode(m_est, C, U_shot, Mtraj_est_n.reshape(n_shots, 6), res, R_pad)
+    #
+    U_shot_full = xp.zeros(s_corrupted.shape)
+    for i in range(len(U_shot)):
+        U_shot_full += eop._gen_U_n(U_shot[i], m_est.shape)
+    #
+    DC = s_n.flatten() - (U_shot_full*s_corrupted).flatten()
+    return xp.abs(xp.dot(xp.conjugate(DC), DC)) #L2-norm
 
 _f_intra_part = partial(_f_intra, m_est = m_est, \
                         C = C, res = res, \
@@ -94,18 +105,53 @@ _f_intra_part = partial(_f_intra, m_est = m_est, \
                         R_pad = R_pad, \
                         s_corrupted = s_corrupted)
 
-f_val = _f_intra_part(Mtraj_est_n)
-g_val = grad(_f_intra_part, Mtraj_est_n)
+def grad(f_init,x,args,h=1e-3):     
+    #CENTRAL FINITE DIFFERENCE CALCULATION
+    # h = xp.cbrt(xp.finfo(float).eps)
+    d = len(x)
+    g = xp.zeros(d)
+    for i in range(d): 
+        #Set-up partial loss func
+        U_shot_i = args[3][i]
+        f = partial(f_init, m_est = args[0], \
+                    C = args[1], res = args[2], \
+                    U_shot = [U_shot_i], \
+                    R_pad = args[4], \
+                    s_corrupted = args[5])
+        #
+        #Evaluate finite difference 
+        x_for = x.at[i].set(x[i]+h) 
+        x_back = x.at[i].set(x[i]-h) 
+        f_for = f(x_for)
+        f_back = f(x_back)
+        f_dif = (f_for- f_back)/(2*h)
+        g = g.at[i].set(f_dif)
+        print("Dimension {} -- Finite dif: {}".format(i+1, f_dif), end = '\r')
+    return g 
 
-from time import time
-t1 = time()
-f_for = f(x_for)
-t2 = time()
-print("Elapsed time: {} sec".format(t2 - t1))
-## NB. takes 4.4 sec per f evaluation
-## So takes 12.8 minutes to eval grad per shot
 
+#--------------------------------
+#Setting up shot pattern 
+nTRs = 16
+nPE = m_est.shape[1]
+U_alt = U[0][1]
+for i in range(1,len(U)):
+    U_alt = xp.concatenate((U_alt, U[i][1]))
 
+nstart = 0
+U_full = []
+U_full.append([U[0][0], U_alt[nstart], U[0][2]])
+for i in range(nstart+1, nPE):
+    U_full.append([U[0][0], U_alt[i], U[0][2]])
+
+#Working with first 16 TRs (ie. first shot)
+shot_ind = 1
+shot_TRs = 16
+U_shot = U_full[shot_ind*shot_TRs:(shot_ind+1)*shot_TRs]
+Mtraj_est_init = Mtraj_est[shot_ind,:]
+Mtraj_est_n = xp.tile(Mtraj_est_init, (shot_TRs,1)).flatten()
+
+#--------------------------------
 #Picking up algorithm
 m_est = np.load(spath + r'/m_intmd.npy')
 m_loss_store = list(np.load(spath + r'/m_loss_store.npy', allow_pickle=1))
@@ -117,8 +163,45 @@ U_shot = U_full[shot_ind*shot_TRs:(shot_ind+1)*shot_TRs]
 Mtraj_est_init = Mtraj_est[shot_ind,:]
 Mtraj_est_n = xp.tile(Mtraj_est_init, (shot_TRs,1)).flatten()
 
-x_opt = BFGS(_f_intra_part,Mtraj_est_n,1)
+maxiter = 2
+args = [m_est, C, res, U_shot, R_pad, s_corrupted]
+g = grad(_f_intra_part, Mtraj_est_n, args)
+opt_out = BFGS(_f_intra_part, Mtraj_est_n, maxiter, spath)
+Mtraj_est_n_new = opt_out[0][0]
+
+Mtraj_est_n_plt = Mtraj_est_n.reshape(16,6)
+Mtraj_est_n_new_plt = Mtraj_est_n_new.reshape(16,6)
+
+plt.style.use('dark_background')
+
+plt.figure()
+plt.plot(Mtraj_est_n_new_plt[:,0], '#1F77B4', label = "SI - UNet+JE")
+plt.plot(Mtraj_est_n_new_plt[:,1], '#2CA02C', label = "AP - UNet+JE")
+plt.plot(Mtraj_est_n_new_plt[:,2], '#E377C2', label = "LR - UNet+JE")
+plt.plot(Mtraj_est_n_plt[:,0], '#1F77B4', linestyle = '--', label = "SI - JE", alpha = 0.90)
+plt.plot(Mtraj_est_n_plt[:,1], '#2CA02C', linestyle = '--', label = "AP - JE", alpha = 0.90)
+plt.plot(Mtraj_est_n_plt[:,2], '#E377C2', linestyle = '--', label = "LR - JE", alpha = 0.90)
+# plt.legend(loc = "lower left")
+plt.xlabel("k-space segment")
+plt.ylabel("Translation (mm)")
+# plt.title("Estimated Translation Parameters")
+plt.title("Translation Parameters")
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.show()
+
+plt.figure()
+plt.plot(Mtraj_est_n_new_plt[:,3], '#1F77B4', label = "SI - UNet+JE")
+plt.plot(Mtraj_est_n_new_plt[:,4], '#2CA02C', label = "AP - UNet+JE")
+plt.plot(Mtraj_est_n_new_plt[:,5], '#E377C2', label = "LR - UNet+JE")
+plt.plot(Mtraj_est_n_plt[:,3], '#1F77B4', linestyle = '--', label = "SI - JE", alpha = 0.90)
+plt.plot(Mtraj_est_n_plt[:,4], '#2CA02C', linestyle = '--', label = "AP - JE", alpha = 0.90)
+plt.plot(Mtraj_est_n_plt[:,5], '#E377C2', linestyle = '--', label = "LR - JE", alpha = 0.90)
+# plt.legend(loc = "lower left")
+plt.xlabel("k-space segment")
+plt.ylabel("Rotation (deg)")
+# plt.title("Estimated Rotation Parameters")
+plt.title("Rotation Parameters")
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.show()
 
 '''
-
-
